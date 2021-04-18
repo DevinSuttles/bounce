82fa7f0e1f7a61d1e4f93cbee450ec5a42d50eef
DevinSuttles <devinfnsuttles@gmail.com>
2021-04-18 00:13:51 -0500

    Final

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const PORT = 3000;
 
 app.use("/assets", express.static('./assets/'));
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname + '/index.html'))
+    res.sendFile(path.join(__dirname + '/index.html'));
 
 });
 
 app.listen(PORT);
-console.log('Express server started');
-console.log(`http://localhost:${PORT}`);
+console.log('Express server runing \n');
+console.log(`Local: http://localhost:${PORT}\n`);
