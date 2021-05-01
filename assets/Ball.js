1abf6f909aaa7c422a4a2715b59722f2915606ba
DevinSuttles <devinfnsuttles@gmail.com>
2021-05-01 15:08:46 -0500

    UI score displayed in nav bar

--- assets/Ball.js
+++ assets/Ball.js
@@ -5,7 +5,9 @@ class Ball{
         this.x_speed = 0;
         this.y_speed = 5;
         this.radius = 5;
+        this.score = 0;
     }
+
     update(paddle1) {
         this.x += this.x_speed;
         this.y += this.y_speed;
@@ -14,41 +16,38 @@ class Ball{
         var bottom_x = this.x + 5;
         var bottom_y = this.y + 5;
       
-        if(this.x - 5 < 0) { // hitting the left wall
+        if(this.x - 5 < 0) {
           this.x = 5;
           this.x_speed = -this.x_speed;
-        } else if(this.x + 5 > 700) { // hitting the right wall
+        } else if(this.x + 5 > 700) {
           this.x = 695;
           this.x_speed = -this.x_speed;
         } 
 
         if (this.y - 5 < 0) {
           this.y = 5;
+          this.x_speed += 1;
           this.y_speed = -this.y_speed;
         }
 
-        if( this.y > 600) { // a point was scored
+        if( this.y > 600) {
           this.x_speed = 0;
           this.y_speed = 3;
-          this.x = 200;
+          this.x = 350;
           this.y = 300;
+          this.score = 0;
+          this.gameOver = true;
+          document.getElementById("navbar-score").innerHTML = this.score;
         }
       
-        if(top_y > 300) {
+        if(top_y > 400) {
           if(top_y < (paddle1.y + paddle1.height) && bottom_y > paddle1.y && top_x < (paddle1.x + paddle1.width) && bottom_x > paddle1.x) {
-            // hit the player's paddle
             this.y_speed = -3;
             this.x_speed += (paddle1.x_speed / 2);
             this.y += this.y_speed;
+            this.score += 1;
+             document.getElementById("navbar-score").innerHTML = this.score;
           }
-        // } else {
-        //   if(top_y < (paddle2.y + paddle2.height) && bottom_y > paddle2.y && top_x < (paddle2.x + paddle2.width) && bottom_x > paddle2.x) {
-        //     // hit the computer's paddle
-        //     this.y_speed = 3;
-        //     this.x_speed += (paddle2.x_speed / 2);
-        //     this.y += this.y_speed;
-        //   }
-        // }
-    }
+        }
   }
 }
--- assets/Paddle.js
+++ assets/Paddle.js
@@ -5,6 +5,34 @@ class Paddle {
         this.width = width;
         this.height = height;
         this.x_speed = 0;
-        this.y_spped = 0;
+        this.y_speed = 0;
+        this.keysDown = {};
+    }
+
+    move(x, y){
+        this.x += x;
+        this.y += y;
+        this.x_speed = x;
+        this.y_speed = y;
+        if (this.x < 0){
+            this.x = 0;
+            this.x_speed = 0;
+        } else if (this.x + this.width > 700){
+            this.x = 700 - this.width;
+            this.x_speed = 0;
+        }
+    }
+
+    update(){
+        for (let key in this.keysDown){
+            let val = Number(key);
+            if (val == 37){
+                this.move(-4, 0);
+            } else if (val == 39){
+                this.move(4, 0);
+            } else {
+                this.move(0,0);
+            }
+        }
     }
 }
